9ed4560f3c8360cd66a7bbb94fdacc672beb3f54
CoffeeKiller <18320210361@163.com>
2023-06-02 23:10:08 +0800

    first commit

---
+++ SparseArray/src/main/java/SparseArray.java
@@ -0,0 +1,16 @@
+
+// 关于稀疏数组的使用
+public class SparseArray {
+    public static void main(String[] args) {
+
+        // 先创建原始数组
+        int[][] arr = new int[11][11];
+        // 并赋值
+        arr[1][2] = 1;
+        arr[2][3] = 2;
+        arr[4][4] = 3;
+
+
+
+    }
+}
