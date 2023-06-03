d7cb59bb36b4a210090cceea5d1d14779171eff7
CoffeeKiller <18320210361@163.com>
2023-06-03 09:02:15 +0800

    create oneway-queue

---
+++ OneWayQueue/src/main/java/OneWayQueue.java
@@ -0,0 +1,14 @@
+import org.apache.log4j.Logger;
+
+// 单向队列
+public class OneWayQueue {
+
+    public static Logger logger = Logger.getLogger(OneWayQueue.class);
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
