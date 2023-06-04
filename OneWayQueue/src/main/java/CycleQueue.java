5ad4d30c8a049be0405ac04dee739b92e248179d
CoffeeKiller <18320210361@163.com>
2023-06-04 18:17:00 +0800

    update

--- OneWayQueue/src/main/java/CycleQueue.java
+++ OneWayQueue/src/main/java/CycleQueue.java
@@ -5,21 +5,72 @@ import java.util.Scanner;
  * */
 public class CycleQueue {
 
-
     private int maxSize; //队列最大值
     private int front;  //队列头
     private int rear;   //队列尾
     private int[] queueArr;
 
-
     public CycleQueue(int maxSize){
-
         this.maxSize = maxSize;
         this.queueArr = new int[maxSize];
-        this.front = -1;
-        this.rear = -1;
+        this.front = 0;
+        this.rear = 0;
+    }
+
+    /**
+     * 判断当前队列是否已满
+     * 说明: 当算法 队列尾+1 % 队列最大个数 等于 队列头 时表示当前队列已满
+     * */
+    public boolean isFull(){
+        return (rear+1) % maxSize == front;
+    }
+
+    /**
+     * 判断当前队列是否为空
+     * 说明: 当队列头与队列尾相同时就表示当前队列为空
+     * */
+    public boolean isEmpty(){
+        return front == rear;
+    }
+
+    /**
+     * 展示当前队列中的有效数据
+     * */
+
+    /**
+     * 往队列中添加元素
+     * */
+    public void add(int number){
+        if (isFull()){
+            System.err.println("当前队列已满,无法添加新的元素");
+        }
+        queueArr[rear] = number;
+        rear = (rear+1) % maxSize;
+    }
+
+    /**
+     * 取出队列中的元素
+     * */
+    public int get(){
+        if (isEmpty()){
+            System.err.println("当前队列已空,无法取出元素");
+            return -1;
+        }
+        int value = queueArr[front];
+        front = (front+1) % maxSize;
+        return value;
+    }
+
+    /**
+     * 展示队列中的所有元素
+     * */
+    public void showAll(){
+        if (isEmpty()){
+            System.err.println("当前队列为空,无法进行展示");
+        }
 
     }
+
 
 
 }
