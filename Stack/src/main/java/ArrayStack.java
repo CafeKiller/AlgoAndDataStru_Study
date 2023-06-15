64134b164f64d1333d9fe5c066795fd068994f99
CoffeeKiller <18320210361@163.com>
2023-06-15 22:51:39 +0800

    stack debug

--- Stack/src/main/java/ArrayStack.java
+++ Stack/src/main/java/ArrayStack.java
@@ -27,8 +27,35 @@ public class ArrayStack {
         return this.top == -1;
     }
 
+    /* 进行入栈操作 */
+    public void push(int item){
+        if (isFull()){
+            System.err.println("当前栈已满,无法进行入栈操作,建议试试pop操作");
+            return;
+        }
+        this.top++;
+        stack[this.top] = item;
+    }
 
+    /* 进行出栈操作 */
+    public int pop(){
+        if (isEmpty()){
+            System.err.println("当前栈为空,无法进行栈操作,可以试试push操作");
+            return 0;
+        }
+        return stack[--this.top];
+    }
 
-
-
+    /* 遍历展示栈中的所有元素 */
+    public void list(){
+        if (isEmpty()){
+            System.err.println("当前栈为空无法展示其中元素");
+            return;
+        }
+        int temp = top;
+        while(temp>=0){
+            System.out.print(stack[temp--] + " ");
+        }
+        System.out.println();
+    }
 }
--- Stack/src/main/java/StackMain.java
+++ Stack/src/main/java/StackMain.java
@@ -1,13 +1,37 @@
+import org.apache.log4j.Logger;
 
 /*
 * 数据结构: 栈
 * */
 public class StackMain {
 
+    public static Logger logger = Logger.getLogger(StackMain.class);
+
     public static void main(String[] args) {
 
+        test1();
 
 
     }
 
+    public static void test1(){
+        logger.warn("当前开始测试数组模拟 栈 的测试");
+        ArrayStack arrayStack = new ArrayStack(5);
+        arrayStack.push(9);
+        arrayStack.push(1);
+        arrayStack.push(3);
+        arrayStack.push(4);
+        arrayStack.push(666);
+
+        arrayStack.list();
+
+        logger.warn("开始进行弹栈操作");
+        arrayStack.pop();
+        arrayStack.pop();
+
+        arrayStack.list();
+    }
+
 }
+
+
