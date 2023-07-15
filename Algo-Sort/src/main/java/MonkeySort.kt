7c8e298406c092501c14dc50325cefb83a346da4
CoffeeKiller <18320210361@163.com>
2023-07-15 14:50:47 +0800

    update

--- Algo-Sort/src/main/java/MonkeySort.kt
+++ Algo-Sort/src/main/java/MonkeySort.kt
@@ -1,13 +1,19 @@
+import kotlin.random.Random
+
 class MonkeySort {
 
-    var arr = arrayOfNulls<Int>(10)
-    var result = arrayOfNulls<Int>(10)
+    private var arr: Array<Int> = arrayOf()
+    private var result = arrayOfNulls<Int>(10)
+
+    constructor(arr: Array<Int>){
+        this.arr = arr
+    }
 
     /**
      * 判断数组是否有序
      * @param arr 需要传入的一个int数组
      * */
-    fun checkOrder(arr:IntArray): Boolean{
+    private fun checkOrder(arr:Array<Int>): Boolean{
         for (i in arr.indices) {
             if (arr[i] <= arr[i-1]){
                 return false
@@ -15,6 +21,25 @@ class MonkeySort {
         }
         return true
     }
+
+    private fun sort(num: Int,random: Random){
+        var numberSet:Set<Int> = HashSet<Int>()
+        println("第"+ num +"次")
+
+        for (i in 0..9){
+            var idx = random.nextInt(10)
+            while (numberSet.contains(idx)){
+                idx = random.nextInt(10)
+            }
+            numberSet.plus(idx)
+            var number = arr[idx]
+            result[i] = number
+            print(" " + result[i])
+        }
+        println()
+    }
+
+
 
 
 
