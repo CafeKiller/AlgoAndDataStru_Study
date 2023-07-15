4c3147dcebfa63795b519d0fc071f1aefeb568e6
CoffeeKiller <18320210361@163.com>
2023-07-15 15:58:40 +0800

    update

--- Algo-Sort/src/main/java/SortMain.java
+++ Algo-Sort/src/main/java/SortMain.java
@@ -15,23 +15,23 @@ public class SortMain {
         InsertSort.sort(arr3);*/
 
         Random random = new Random();
-
-        Integer[] arr = new Integer[]{1,2,3,4,5,6,7,8,9,10};
+        Integer[] arr = new Integer[]{9,4,7,1,3,5,10,2,6,8};
         MonkeySort monkeySort = new MonkeySort(arr);
-
+        boolean isOrder = false;
         int num = 0;
-        long startTime = System.currentTimeMillis();
 
+        // 开始
+        long startTime = System.currentTimeMillis();
         while(true){
             monkeySort.sort(num++,random);
-            boolean isOrder = monkeySort.checkOrder(monkeySort.getResult());
-
+            isOrder = monkeySort.checkOrder(monkeySort.getResult());
             if (isOrder){
                 break;
             }
-
         }
         long endTime = System.currentTimeMillis();
+        // 结束
+
         System.out.println("排序完成，约耗时： " + (endTime - startTime)/1000 + "S");
         System.out.printf("一共执行了 %d 次\n",num);
 
